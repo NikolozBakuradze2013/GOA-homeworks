329c12f900f02b9262313120a6fc7724fa10796e
Nikoloz Bakuradze <kolozi2013@gmail.com>
2025-08-24 01:55:54 +0700

    Add files via upload

---
+++ cw_67.py
@@ -0,0 +1,18 @@
+# ცვლადებს ვიყენებთ როგორც ყუთი. ცვლადებში ჩვენ ვინახავთ მონაცემებს.
+
+num1 = 86
+num2 = 2
+print (num1 + num2)
+print (num1 - num2)
+print (num1 * num2)
+print (num1 / num2)
+
+gvari = "bakuradze"
+asaki = 11
+print (gvari * asaki)
+
+name = "nikoloz"
+surname = "bakuradze"
+age = 11
+height = 1.67
+print ("gamarjoba, chemi saxelia", name, "chemi gvaria", surname, "me var", age, "me var", height, "cm")
---
+++ hw_67.py
@@ -0,0 +1,35 @@
+#1)შევქმნათ 2 ცვლადი მათში მოვათავსეთ integer (მთელი რიცხვი) მონაცემთა ტიპები და მოახდინეთ ყველა მათემატიკური ოპერატორი ასევე კომენტარების სახით ახსენით რას აკეთებს თითოეული ოპერატორი
+
+num1 = 88
+num2 = 54
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+#2)შევქმნათ 1 ცვლადი, სახელად name შეინახავ string (მონაცემთა ტიპს) შემდგომ ამ name ცვლადში შეინახე შენი სახელი და ეს სახელი გაამრავლე 11 ზე კომენტარის სახით მიაწერე რა გამოვა output:
+
+name = "nikoloz"
+print(name * 11)
+
+#nikoloznikoloznikoloznikoloznikoloznikoloznikoloznikoloznikoloznikoloznikoloz
+#3)შევქმნათ 2 ცვლადი პირველი იყოს name სადაც შეინახავ შენს სახელს მეორე იყოს surname სადაც შეინახავ შენს გვარს შემდეგ + ის გამოყენებით მოახდინეთ მათზე კონკატინაცია ანუ (ორი სტრინგის შეერთება)
+
+name = "nikoloz"
+surname = "bakuradze"
+print(name + surname)
+
+#4)გამოიტანეთ ტერმინალში თქვენი სახელი და გვარი 100-ჯერ(გამოიყენეთ გამრავლება)
+
+fullname = "nikolozbakuradze"
+print(fullname * 100)
+
+#5)კომენტარებით ახსენით რა არის კონკატინაცია. მოიყვანეთ მაგალითები
+
+#კონკატინაცია არის როდესაც 2 stringს ატყუპებ. მაგალითად print("nikoloz" + "bakuradze")
+
+#6)გამოასწორეთ შემდეგი კოდი:
+#city => "London':
+#print"city)
+city = "London"
+print(city)
